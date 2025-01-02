61a406fcd45440e3537b00a061285e8992a7dfeb
ShanifKn <shanifkaraden06@gmail.com>
2025-01-02 17:09:23 +0530

    lated code

--- src/app/pages/home/mega-menu/mega-menu.component.ts
+++ src/app/pages/home/mega-menu/mega-menu.component.ts
@@ -10,61 +10,7 @@ import { menuData } from '../data';
   styleUrls: ['./mega-menu.component.scss'],
 })
 export class MegaMenuComponent {
-  categories = [
-    { title: 'Fresh Food' },
-    { title: 'Grocery' },
-    { title: 'Grocery Non Food' },
-    { title: 'Housewares' },
-    { title: 'Stationary' },
-    { title: 'Electronics' },
-    { title: 'Garden' },
-    { title: 'Camping' },
-  ];
-
-  subCategories = [
-    {
-      title: 'Fresh Food',
-      subCategory: [
-        { title: 'Vegetables' },
-        { title: 'Fruits' },
-        { title: 'Meat' },
-      ],
-    },
-    {
-      title: 'Grocery',
-      subCategory: [
-        { title: 'Rice & Grains' },
-        { title: 'Spices' },
-        { title: 'Oil' },
-      ],
-    },
-    {
-      title: 'Electronics',
-      subCategory: [
-        { title: 'Mobile Phones' },
-        { title: 'Laptops' },
-        { title: 'Accessories' },
-      ],
-    },
-  ];
-
-  hoveredSubCategories: any[] | null = null;
-
   menuData = menuData; // Imported menu data
-  onHover(category: any) {
-    const found = this.subCategories.find(
-      (subCategory) => subCategory.title === category.title
-    );
-
-    console.log(found);
-
-    this.hoveredSubCategories = found ? [found] : null;
-  }
-
-  resetHover() {
-    this.hoveredSubCategories = null;
-  }
-
   activeCategory: string | null = null; // Tracks active category
   activeSubcategory: string | null = null; // Tracks active subcategory
 
